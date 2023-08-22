0bde1dcf95e01abf86749aecad8b5a9df0da64e7
Jerome Doran <disneyfan9184@gmail.com>
2023-08-22 15:50:29 -0400

    added routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/home';
+import AboutPage from './pages/About';
+import SignInPage from './pages/signin';
+import SignUpPage from './pages/signup';
+import ProfilePage from './pages/profile';
+
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/about' element={<AboutPage />} />
+        <Route path='/sign-in' element={<SignInPage />} />
+        <Route path='/sign-up' element={<SignUpPage />} />
+        <Route path='/profile' element={<ProfilePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
