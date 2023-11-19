db25bf5293fa65f06e3f9b2e7b107d6785f23fb6
Jerome Doran <disneyfan9184@gmail.com>
2023-11-19 10:41:17 -0500

    updated clear cookies from browser

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -42,8 +42,8 @@ export async function deleteUser(req, res, next) {
   }
   try {
     await User.findByIdAndDelete(req.params.id);
-
-    res.status(200).json('User has been deleted.').clearCookie('access_token');
+    res.clearCookie('access_token');
+    res.status(200).json('User has been deleted.');
   } catch (error) {
     next(error);
   }
